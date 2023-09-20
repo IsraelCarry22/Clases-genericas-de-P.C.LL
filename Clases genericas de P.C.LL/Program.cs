557702558ada1a866117076bde1083722c89cda0
Israel Carreon <Israelcarreon992@gmail.com>
2023-09-20 15:14:18 -0600

    Agregar archivos de proyecto.

---
+++ Clases genericas de P.C.LL/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_genericas_de_P.C.LL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Pila
+            int res = 0;
+            Stack<int> Mi_pila = new Stack<int>();
+            Mi_pila.Push(1);
+            Mi_pila.Push(20);
+            Mi_pila.Push(13);
+            Mi_pila.Push(4);
+            Mi_pila.Push(53);
+            Console.WriteLine(" == Mi pila == ");
+            foreach (var i in Mi_pila)
+            {
+                Console.WriteLine("-- " + i);
+                res += i;
+            }
+            Console.WriteLine("La suma de los elemntos de la pila es: " + res);
+            Console.ReadKey();
+            #endregion
+
+            #region Cola
+            Console.Clear();
+            Queue<double> Mi_cola = new Queue<double>();
+            Mi_cola.Enqueue(1.5);
+            Mi_cola.Enqueue(12.4);
+            Mi_cola.Enqueue(32.8);
+            Mi_cola.Enqueue(.89);
+            Mi_cola.Enqueue(18.9);
+            Console.WriteLine("== Mi cola == ");
+            foreach (var i in Mi_cola)
+            {
+                Console.WriteLine("-- " + i);
+            }
+            Console.ReadKey();
+            #endregion
+
+            #region Lista_enlazada
+            Console.Clear();
+            LinkedList<string> Mi_linked_list = new LinkedList<string>();
+            Mi_linked_list.AddLast("A");
+            Mi_linked_list.AddLast("B");
+            Mi_linked_list.AddLast("C");
+
+            Console.WriteLine("== Lista enlazada == ");
+            foreach (var item in Mi_linked_list)
+            {
+                Console.WriteLine("-- " + item);
+            }
+
+            LinkedListNode<string> nodo = Mi_linked_list.Find("B");
+            if (nodo != null)
+            {
+                Mi_linked_list.AddAfter(nodo, "D");
+            }
+
+            Console.WriteLine("\n== Lista enlazada después de insertar 'D' ==");
+            foreach (var item in Mi_linked_list)
+            {
+                Console.WriteLine("-- " + item);
+            }
+            Console.ReadKey();
+            #endregion
+        }
+    }
+}
